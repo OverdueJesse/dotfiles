8bde2ee8dc34968bcd8f021abc62bc3d978e8549
Jesse Guillory <jhowder91@gmail.com>
2025-11-27 15:21:12 -0600

    Add disk management

--- .nix/configuration.nix
+++ .nix/configuration.nix
@@ -147,6 +147,10 @@ in
     "$HOME/.config/rofi/scripts"
   ];
 
+  # mounting disks
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
